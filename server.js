b32fe5f4ff6fa047c8656ad7f19e4247aeff0649
Emilio Crocco <3357168+Dellrodar@users.noreply.github.com>
2020-08-05 16:06:21 -0400

    All endpoints working * All endpoints are working * Error handling still being worked on.

--- server.js
+++ server.js
@@ -15,22 +15,49 @@ server.get("/api/users", (req, res) => {
   res.json(users);
 })
 
-server.post("api/users", (req, res) =>{
+server.post("/api/users", (req, res) =>{
   const newUser = db.createUser({
     name: req.body.name,
     bio: req.body.bio,
   })
+  if (newUser) {
   res.status(201).json(newUser)
+  } else {
+    status(500).json({ errorMessage: "There was an error while saving the user to the database" })
+  }
 })
 
 server.get("/api/users/:id", (req, res) => {
-  const id = req.params.id,
-  const user = db.getUserById(id)
-
+  const user = db.getUserById(req.params.id)
   if (user) {
-    req.json(user)
+    res.json(user)
   } else {
     res.status(500).json({ errorMessage: "The users information could not be retrieved." })
   }
 })
 
+server.delete("/api/users/:id", (req, res) => {
+  const user = db.getUserById(req.params.id)
+
+  if (user) {
+    db.deleteUser(req.params.id)
+    res.status(204).end()
+  } else {
+    res.status(404).json({ message: "The user with the specified ID does not exist." })
+  }
+})
+
+ server.put("/api/users/:id", (req, res) => {
+  const user = db.getUserById(req.params.id)
+  if (user) {
+    db.updateUser(user.id, {
+      name: req.body.name || user.name,
+      bio: req.body.bio || user.bio,
+    })
+    res.status(200).json({message: "The user has been Updated!"})
+  }
+ })
+
+ server.listen(8080, () =>{
+  console.log("Server started on port 8080")
+})
